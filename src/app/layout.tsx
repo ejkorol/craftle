8bdac543889c311a89c839cd67c51fc666e81f56
Jason Korol <ejkorol@gmail.com>
2024-08-01 18:49:32 -0400

    add toaster to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Providers } from "@/providers/providers";
 import { inter, mononokiFont, karnakPro } from "@/utils/fonts";
+import { Toaster } from "react-hot-toast";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,6 +14,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     <html lang="en" className="dark" suppressHydrationWarning>
       <body className={`${inter.className} ${mononokiFont.variable} ${karnakPro.variable}`}>
         <Providers>
+          <Toaster position="top-center" />
           {children}
         </Providers>
       </body>
