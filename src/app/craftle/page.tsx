f1158fc9aef84561c8452dc74024c803b183301d
Jason Korol <ejkorol@gmail.com>
2024-08-02 20:15:14 -0400

    fetch recipe based on date

--- src/app/craftle/page.tsx
+++ src/app/craftle/page.tsx
@@ -1,13 +1,9 @@
 import CraftingTable from "../components/CraftingTable";
+import dailyRecipes from "@/utils/dailyRecipes";
 
 const fetchItems = async () => {
   try {
-    const res = await fetch(`${process.env.API_URL}/api/item`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
+    const res = await fetch(`${process.env.API_URL}/api/item`);
     const data = await res.json();
     return data;
   } catch (e) {
@@ -17,12 +13,10 @@ const fetchItems = async () => {
 
 const fetchRecipe = async () => {
   try {
-    const res = await fetch(`${process.env.API_URL}/api/recipe`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
+    const d = new Date();
+    const today = (d.toISOString().split('T')[0]);
+    const todaysRecipe = dailyRecipes[`${today}`];
+    const res = await fetch(`${process.env.API_URL}/api/recipe/${todaysRecipe}`, { cache: 'no-cache' });
     const data = await res.json();
     return data;
   } catch (e) {
