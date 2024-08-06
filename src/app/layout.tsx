27a50157c995faeb8abb2cd1c6a128e6dc409782
Jason Korol <ejkorol@gmail.com>
2024-08-05 23:17:52 -0400

    create metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,20 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Craftle",
   description: "A minimalist Minecraft-themed Wordle-like clone inspired by the New York Times' popular game.",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["wordle", "minecraft", "craft"],
+  authors: [
+    { name: "Jason Korol" },
+    {
+      name: "Jason Korol",
+      url: "https://www.linkedin.com/in/ejkorol"
+    },
+  ],
+  icons: [
+    { rel: "apple-touch-icon", url: "apple-touch-icon.png" },
+    { rel: "icon", url: "favicon.ico" },
+  ],
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
